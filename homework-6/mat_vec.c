945dca1d22030193c431fb852b3454a4d6cd6b2d
Alex Vallejo <vallejo.alex@gmail.com>
2014-04-09 11:51:58 -0400

    Reformat mat_vec.c. Add number of threads as a command line argument

--- homework-6/mat_vec.c
+++ homework-6/mat_vec.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- *  Matrix vector multiplication using one thread
+ *  Matrix vector multiplication using N threads
  *****************************************************************************/
 
 #include <stdio.h>
@@ -7,15 +7,21 @@
 #include <math.h>
 #include <sys/time.h>
 
-int N ;
-float** a ;
-float* b ;
-float* x ;
+int N, num_threads;
+float** a;
+float* b;
+float* x;
 
-int main (int argc, char *argv[] )    {
-/* the array size should be supplied as a command line argument */
-  if(argc != 2) {printf("wrong number of arguments") ; exit(2) ;}
-  N = atoi(argv[1]) ;
+int main(int argc, char *argv[] ){
+  // the array size and number of execution threads should be supplied as a
+  // command line argument
+  if (argc != 3){
+    printf("wrong number of arguments");
+    exit(2);
+  }
+
+  N = atoi(argv[1]);
+  num_threads = atoi(argv[2]);
   printf("Array size = %d \n ", N );
   int mid = (N+1)/2;
   int i, j;
@@ -23,27 +29,32 @@ int main (int argc, char *argv[] )    {
   struct timeval tv;
   struct timezone tz;
 
-/* allocate arrays dynamically */
+  // allocate arrays dynamically
   a = malloc(sizeof(float*)*N);
   for (i = 0; i < N; i++) {
-		a[i] = malloc(sizeof(float)*N);
+    a[i] = malloc(sizeof(float)*N);
   }
+
   b = malloc(sizeof(float)*N);
   x = malloc(sizeof(float)*N);
 
-  /* Inititialize matrix A and vector B. */
+  // Inititialize matrix A and vector B
   for (i=0; i<N; i++) {
     for (j=0; j<N; j++) {
-      if (j == i)                    { a[i][j] = 2.0; }
-      else if (j == i-1 || j == i+1) { a[i][j] = 1.0; }
-      else                           { a[i][j] = 0.01; }
+      if (j == i)
+        a[i][j] = 2.0;
+
+      else if (j == i-1 || j == i+1)
+        a[i][j] = 1.0;
+
+      else
+        a[i][j] = 0.01;
     }
     b[i] = mid - abs(i-mid+1);
   }
 
-  gettimeofday (&tv ,   &tz);
-  time_start = (double)tv.tv_sec +
-            (double)tv.tv_usec / 1000000.0;
+  gettimeofday(&tv, &tz);
+  time_start = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.0;
 
     for (i=0; i<N; i++) x[i] = 0.0;
     for (i=0; i<N; i++) {
@@ -52,13 +63,12 @@ int main (int argc, char *argv[] )    {
       }
     }
 
-  gettimeofday (&tv ,  &tz);
-  time_end = (double)tv.tv_sec +
-          (double)tv.tv_usec / 1000000.0;
+  gettimeofday(&tv, &tz);
+  time_end = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.0;
 
-/* this is for checking the results */
-  for (i=0; i<N; i+=N/10) { printf(" %10.6f",x[i]); }
-  printf("\n");
-  printf ("time = %lf\n", time_end - time_start);
-
+  // this is for checking the results
+  for (i=0; i<N; i+=N/10)
+    printf(" %10.6f",x[i]);
+  printf("\ntime = %lf\n", time_end - time_start);
 }
+
