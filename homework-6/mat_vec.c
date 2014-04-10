48ae6248c3d1b806c4a48586c9a2f466eba9eca1
Alex Vallejo <vallejo.alex@gmail.com>
2014-04-10 01:19:08 -0400

    Implement muti-threaded application

--- homework-6/mat_vec.c
+++ homework-6/mat_vec.c
@@ -2,15 +2,35 @@
  *  Matrix vector multiplication using N threads
  *****************************************************************************/
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <pthread.h>
 #include <sys/time.h>
 
 int N, num_threads;
 float** a;
 float* b;
 float* x;
+
+void mm(int id) {
+  int i, j, k;
+  double sum;
+
+  // compute bounds for this threads---just algebra
+  int startrow = id * N / num_threads;
+  int endrow = (id + 1) * (N / num_threads) - 1;
+
+  for (i = startrow; i < endrow; i++)
+    for (j = 0; j < N; j++)
+      x[i] += a[i][j] * b[j];
+}
+
+void *worker(void *arg){
+  int id = *((int *) arg);
+  mm(id);
+  return 0;
+}
 
 int main(int argc, char *argv[] ){
   // the array size and number of execution threads should be supplied as a
@@ -20,11 +40,13 @@ int main(int argc, char *argv[] ){
     exit(2);
   }
 
+  pthread_t *threads;
   N = atoi(argv[1]);
   num_threads = atoi(argv[2]);
   printf("Array size = %d \n ", N );
   int mid = (N+1)/2;
   int i, j;
+  int *p;
   double time_start, time_end;
   struct timeval tv;
   struct timezone tz;
@@ -56,12 +78,22 @@ int main(int argc, char *argv[] ){
   gettimeofday(&tv, &tz);
   time_start = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.0;
 
-    for (i=0; i<N; i++) x[i] = 0.0;
-    for (i=0; i<N; i++) {
-      for (j=0; j<N; j++) {
-         x[i] += a[i][j] * b[j];
-      }
-    }
+  for (i=0; i<N; i++) 
+    x[i] = 0.0;
+
+  // Allocate thread handles
+  threads = (pthread_t *) malloc(num_threads * sizeof(pthread_t));
+
+  // Create threads
+  for (i = 0; i < num_threads; i++) {
+    p = (int *) malloc(sizeof(int));  // yes, memory leak, don't worry for now
+    *p = i;
+    pthread_create(&threads[i], NULL, worker, (void *)(p));
+  }
+
+  for (i = 0; i < num_threads; i++) {
+    pthread_join(threads[i], NULL);
+  }
 
   gettimeofday(&tv, &tz);
   time_end = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.0;
@@ -69,6 +101,6 @@ int main(int argc, char *argv[] ){
   // this is for checking the results
   for (i=0; i<N; i+=N/10)
     printf(" %10.6f",x[i]);
+  
   printf("\ntime = %lf\n", time_end - time_start);
 }
-
